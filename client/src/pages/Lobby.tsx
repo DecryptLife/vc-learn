cee97faf900f3e99839a3a2ada602a2932b4e27e
DecryptLife <benson.thomas2602@gmail.com>
2023-12-27 20:33:30 -0600

    Logic for joining room

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,21 +1,36 @@
 // @ts-check
 
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSocket } from "../context/SocketProvider";
 
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
+  const navigate = useNavigate();
+  const socket = useSocket();
 
   const handleSubmit = useCallback(
     (e: any) => {
       e.preventDefault();
-      console.log({
-        email,
-        room,
-      });
+      socket?.emit("room:join", { email, room });
     },
     [email, room]
   );
+
+  const handleJoinRoom = useCallback((data: any) => {
+    const { email, room } = data;
+    navigate(`/room/${room}`);
+  }, []);
+
+  useEffect(() => {
+    socket?.on("room:join", handleJoinRoom);
+
+    return () => {
+      socket?.off("room:join", handleJoinRoom);
+    };
+  }, [socket, handleJoinRoom]);
+
   return (
     <div>
       <h1>Lobby</h1>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const { Server } = require("socket.io");
+const { Server, Socket } = require("socket.io");
 
 const io = new Server(3001, {
   cors: true,
 });
 
+const EmailToSocketId = new Map();
+const SocketIdToEmail = new Map();
+
 io.on("connection", (socket) => {
   console.log("Socket Connected", socket.id);
+
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+
+    EmailToSocketId.set(email, socket.id);
+    SocketIdToEmail.set(socket.id, email);
+
+    io.to(socket.id).emit("room:join", data);
+  });
 });
