ba39d99d6c208f26e20daed6dcdae4da6f63d8e9
DecryptLife <benson.thomas2602@gmail.com>
2023-12-27 19:09:28 -0600

    lobby screen updated

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,17 +1,42 @@
 // @ts-check
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      console.log({
+        email,
+        room,
+      });
+    },
+    [email, room]
+  );
   return (
     <div>
       <h1>Lobby</h1>
-      <form className="lobby_form">
+      <form className="lobby_form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email ID</label>
-        <input type="email" name="email" id="uemail" />
+        <input
+          type="email"
+          name="email"
+          id="uemail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="room_num">Room Number</label>
-        <input type="number" name="room_num" id="room_num" />
+        <input
+          type="number"
+          name="room_num"
+          id="room_num"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
 
         <button>Join</button>
       </form>
