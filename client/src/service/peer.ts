3ccfcc55d6d1cd35fb0466ef5e1d3fee2b4b59d6
DecryptLife <benson.thomas2602@gmail.com>
2023-12-30 00:37:41 -0600

    peer service added

---
+++ client/src/service/peer.ts
@@ -0,0 +1,26 @@
+// @ts-check
+
+class PeerService {
+  peer: RTCPeerConnection | null = null;
+
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
+      });
+    }
+  }
+
+  async getOffer() {
+    const offer = await this.peer?.createOffer();
+    await this.peer?.setLocalDescription(offer);
+    return offer;
+  }
+}
